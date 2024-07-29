3a2e140d566d69567901e4002fbef4498dc848dc
Damir2302 <damir23.02@mail.ru>
2024-07-29 18:55:30 +0300

    fixed menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -35,65 +35,110 @@ Inputmask({
 }).mask('[number-only]');
 
 // Burger menu
-// $('#burger').on('click', (e) => {
-//     $('#header').append('<div id="mobile-menu" class="mobile-menu"></div>');
-//     $('#mobile-menu').append('');
-// });
+$('#burger').on('click', (e) => {
+    // Check if the mobile menu already exists
+    if ($('#mobile-menu').length === 0) {
+        const $mobileMenu = $('<div id="mobile-menu" class="mobile-menu"></div>');
+        $('#header').append($mobileMenu);
+
+        $mobileMenu.append($('.header__search').clone(true));
+        $mobileMenu.append($('.header__category').clone(true));
+        $mobileMenu.append($('.header__nav').clone(true));
+        $mobileMenu.append($('.header__selection').clone(true));
+
+        mobileSubmenu();
+    }
+
+    const $header = $('#header');
+    const $mobileMenu = $('#mobile-menu');
+
+    if (!$header.hasClass('mobile-menu-active')) {
+        $header.addClass('mobile-menu-active');
+        $mobileMenu.fadeIn(200);
+    } else {
+        $header.removeClass('mobile-menu-active');
+        $mobileMenu.fadeOut(200);
+    }
+});
 
 // Mobile-menu submenu
-const mobieSubmenu = () => {
-    if (!checkIsDesktop()) {
-        $('#mobile-menu .header__category-item > a').on('click', (e) => {
-            e.preventDefault();
+const mobileSubmenu = () => {
+    const $mobileMenu = $('#mobile-menu');
+    const $categoryItems = $mobileMenu.find('.header__category-item > a');
+    const $menuItems = $mobileMenu.find('.header__menu-item.has-child > a');
 
-            if ($(e.currentTarget).parent().hasClass('active')) {
-                $(e.currentTarget).closest('#mobile-menu').removeClass('expanded');
-                $(e.currentTarget).parent().removeClass('active');
-                $('#mobile-menu .header__category-item').show();
-                $('#mobile-menu .header__search').show();
-                $('#mobile-menu .header__nav').show();
-                $('#mobile-menu .header__selection').show();
-            } else {
-                $(e.currentTarget).closest('#mobile-menu').addClass('expanded');
-                $(e.currentTarget).parent().addClass('active');
-                $('#mobile-menu .header__category-item:not(.active)').hide();
-                $('#mobile-menu .header__search').hide();
-                $('#mobile-menu .header__nav').hide();
-                $('#mobile-menu .header__selection').hide();
+    $categoryItems.off('click').on('click', (e) => {
+        e.preventDefault();
+        toggleSubMenu($(e.currentTarget).parent(), '.header__category-item');
+    });
+
+    $menuItems.off('click').on('click', (e) => {
+        e.preventDefault();
+        toggleSubMenu($(e.currentTarget).parent(), '.header__menu-item');
+    });
+
+    function toggleSubMenu($item, itemSelector) {
+        if (!$item.hasClass('active')) {
+            $mobileMenu.addClass('expanded');
+            $item.addClass('active');
+            $mobileMenu.find(`${itemSelector}:not(.active)`).hide();
+            $mobileMenu.find('.header__search, .header__nav, .header__selection').hide();
+        } else {
+            $mobileMenu.removeClass('expanded');
+            $item.removeClass('active');
+            $mobileMenu.find(`${itemSelector}`).show();
+            $mobileMenu.find('.header__search, .header__nav, .header__selection').show();
+        }
+    }
+
+    // Check on resize
+    $(window)
+        .off('resize.mobileSubmenu')
+        .on('resize.mobileSubmenu', () => {
+            if (!checkIsDesktop()) {
+                mobileSubmenu();
             }
         });
+};
 
-        $('#mobile-menu .header__menu-item.has-child > a').on('click', (e) => {
-            e.preventDefault();
+// Initial call
+mobileSubmenu();
 
-            if ($(e.currentTarget).parent().hasClass('active')) {
-                $(e.currentTarget).closest('#mobile-menu').removeClass('expanded');
-                $(e.currentTarget).parent().removeClass('active');
-                $('#mobile-menu .header__menu-item').show();
-                $('#mobile-menu .header__category').show();
-                $('#mobile-menu .header__search').show();
-                $('#mobile-menu .header__bar').show();
-                $('#mobile-menu .header__selection').show();
-            } else {
-                $(e.currentTarget).closest('#mobile-menu').addClass('expanded');
-                $(e.currentTarget).parent().addClass('active');
-                $('#mobile-menu .header__menu-item:not(.active)').hide();
-                $('#mobile-menu .header__category').hide();
-                $('#mobile-menu .header__search').hide();
-                $('#mobile-menu .header__bar').hide();
-                $('#mobile-menu .header__selection').hide();
+// Search button (mobile)
+$('.header__main .search-icon').on('click', (e) => {
+    const $headerSearch = $(e.currentTarget).closest('.header__search');
+    const $inputSearch = $headerSearch.find('.input-search');
+
+    if (
+        !checkIsDesktop() &&
+        $(e.currentTarget).closest('.header__main').length &&
+        !$headerSearch.hasClass('active')
+    ) {
+        e.preventDefault();
+        $headerSearch.addClass('active');
+        $inputSearch.addClass('focus');
+        $inputSearch.find('input').focus();
+
+        // Обработчик клика по кнопке очистки поля ввода
+        $('.input-search__clear').one('click', () => {
+            $inputSearch.removeClass('focus');
+            $headerSearch.removeClass('active');
+        });
+
+        // Обработчик клика по документу для закрытия поиска при клике вне его области
+        $(document).one('click', (e) => {
+            if (
+                !$(e.target).closest('.input-search').length &&
+                !$(e.target).closest('.search-icon').length
+            ) {
+                $inputSearch.removeClass('focus');
+                $headerSearch.removeClass('active');
             }
         });
     }
-};
-
-mobieSubmenu();
-
-$(window).on('resize', (e) => {
-    mobieSubmenu();
 });
 
-// Quantity buttons
+// Для кнопок "-" и "+"
 $('.qty-input .qty').on('click', (e) => {
     if (
         $(e.currentTarget).hasClass('qty-minus') &&
